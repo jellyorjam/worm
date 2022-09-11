9e99209608de6749e99eed7e63f1b9b8a992a671
Emily Freeman <freeman.ecf@gmail.com>
2022-09-11 14:28:28 -0400

    fix url error

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    url: "https://wormworm.herokuapp.com/"
+    url: "https://wormworm.herokuapp.com"
   }
 } else {
   module.exports = {
